d4955b675cb6cf1f7b7c555dc6de24a6014bb895
Rafael Tafelli <rafaeltafellisantos@gmail.com>
2024-10-14 19:10:43 -0300

    Primeira versão API

---
+++ api/constantes.py
@@ -0,0 +1,32 @@
+HEADER = {'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36'}
+
+
+LINK_VITIBRASIL = r'http://vitibrasil.cnpuv.embrapa.br/index.php'
+
+
+ABA_PRODUCAO = r'opcao=opt_02'
+
+
+ABA_PROCESSAMENTO = r'opcao=opt_03'
+ABA_PROCESSAMENTO_SUBOPCAO_VINIFERAS = r'subopcao=subopt_01'
+ABA_PROCESSAMENTO_SUBOPCAO_AMERICANAS_HIBRIDAS = r'subopcao=subopt_02'
+ABA_PROCESSAMENTO_SUBOPCAO_UVAS_MESA = r'subopcao=subopt_03'
+ABA_PROCESSAMENTO_SUBOPCAO_SEM_CLASSIFICACAO = r'subopcao=subopt_04'
+
+
+ABA_COMERCIALIZACAO = r'opcao=opt_04'
+
+
+ABA_IMPORTACAO = r'opcao=opt_05'
+ABA_IMPORTACAO_SUBOPCAO_VINHOS_MESA = r'subopcao=subopt_01'
+ABA_IMPORTACAO_SUBOPCAO_ESPUMANTES = r'subopcao=subopt_02'
+ABA_IMPORTACAO_SUBOPCAO_UVAS_FRESCAS = r'subopcao=subopt_03'
+ABA_IMPORTACAO_SUBOPCAO_UVAS_PASSAS = r'subopcao=subopt_04'
+ABA_IMPORTACAO_SUBOPCAO_SUCO_UVA = r'subopcao=subopt_05'
+
+
+ABA_EXPORTACAO = r'opcao=opt_06'
+ABA_EXPORTACAO_SUBOPCAO_VINHOS_MESA = r'subopcao=subopt_01'
+ABA_EXPORTACAO_SUBOPCAO_ESPUMANTES = r'subopcao=subopt_02'
+ABA_EXPORTACAO_SUBOPCAO_UVAS_FRESCAS = r'subopcao=subopt_03'
+ABA_EXPORTACAO_SUBOPCAO_SUCO_UVA = r'subopcao=subopt_04'
